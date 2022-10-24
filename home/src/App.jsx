dc61c076a7ba1058a6f9defbfef07544d06f9624
Peter Chu <peterc0523@gmail.com>
2022-10-24 19:46:37 -0400

    add server

--- home/src/App.jsx
+++ home/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Header from './Header';
 import Footer from './Footer';
+import HomeContent from './HomeContent';
 
 import "./index.scss";
 
@@ -9,7 +10,7 @@ const App = () => (
   <div className="text-3xl mx-auto max-w-6xl">
     <Header />
     <div className='my-10'>
-      Home Page Content
+      <HomeContent />
     </div>
     <Footer />
   </div>
--- pdp/src/App.jsx
+++ pdp/src/App.jsx
@@ -1,20 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from 'home/Header';
 import Footer from 'home/Footer';
-import SafeComponent from "./SafeComponent";
+import PDPContent from "./PDPContent";
 import "./index.scss";
 
 const App = () => (
-  <div className="text-3xl mx-auto max-w-6xl">
-    <SafeComponent>
+  <Router>
+    <div className="text-3xl mx-auto max-w-6xl">
       <Header />
-    </SafeComponent>
-    <div className='my-10'>
-      PDP Page Content
+      <div className='my-10'>
+        <Routes>
+          <Route path='product/:id' element={<PDPContent />} />
+        </Routes>
+        <PDPContent />
+      </div>
+      <Footer />
     </div>
-    <Footer />
-  </div>
+  </Router>
 );
 
 ReactDOM.render(<App />, document.getElementById("app"));
